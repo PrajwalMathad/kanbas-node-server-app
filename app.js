faac1fbf9a65458027b98b57fb6849bef8c59a75
PrajwalMathad <prajwalmathad966@gmail.com>
2023-11-13 23:42:15 -0500

    HTTP server

---
+++ app.js
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import cors from "cors";
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+    })
+);
+app.use(express.json());
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
