1e1d5b23f819f58f2f83b9bddb69577fd1275d35
PrajwalMathad <prajwalmathad966@gmail.com>
2023-11-14 14:19:06 -0500

    Module rotes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 const app = express();
 app.use(
@@ -13,6 +14,7 @@ app.use(
 );
 app.use(express.json());
 CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
