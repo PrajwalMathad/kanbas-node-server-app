5f9bf4651b5128c5e8f1a602ce5b64939b6985eb
PrajwalMathad <prajwalmathad966@gmail.com>
2023-12-03 14:47:26 -0500

    Mogo Db integration

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import UserRoutes from "./users/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(
     cors({
@@ -17,6 +20,7 @@ app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(process.env.PORT || 4000);
